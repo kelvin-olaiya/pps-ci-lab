2a1bdbbd5bb5e9b6339137330a2b4a403920fb09
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-05-02 18:19:17 +0200

    improve good scala 3 library

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,5 +13,5 @@ repositories {
 }
 
 dependencies {
-    implementation("org.scala-lang:scala-library:2.11.12")
+    implementation("org.scala-lang:scala3-library_3:3.3.1-RC1-bin-20230215-006e2e4-NIGHTLY")
 }
